f81d3431d7b0aed15d14b3dc1ca5b517a2b63cab
KoYunHyuk <59502729+KoYunHyuk@users.noreply.github.com>
2024-11-16 03:36:50 +0900

    Definition of Object Class

---
+++ src/lang/langMain.java
@@ -0,0 +1,8 @@
+package lang;
+
+public class langMain {
+
+    public static void main(String[] args) {
+        System.out.println("hello java");
+    }
+}
---
+++ src/lang/object/Child.java
@@ -0,0 +1,9 @@
+package lang.object;
+
+// Parent 클래스 상속 명시적 호출
+public class Child extends Parent {
+
+    public void childMethod() {
+        System.out.println("Child.childMethod");
+    }
+}
